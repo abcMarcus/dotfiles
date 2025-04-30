16e5f9e9f4fa41d8190a7944d3f1d444a406f9f4
abcMarcus <marcus.dhillon921@gmail.com>
2025-05-01 01:33:03 +1000

    tmux

--- time.sh
+++
@@ -1,5 +0,0 @@
-echo "\
-#[fg=gray]HKG #[fg=white]$(TZ='Hongkong' date +%R | tr -d '\n') \
-#[fg=gray]LND #[fg=white]$(TZ='Europe/London' date +%R | tr -d '\n') \
-#[fg=gray]NYC #[fg=white]$(TZ='America/New_York' date +%R | tr -d '\n')"
-
